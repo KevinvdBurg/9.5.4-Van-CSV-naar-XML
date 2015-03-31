468eeaf792c36c426024026ccc3d1a6d415881eb
salami350 <salami350@live.nl>
2015-03-31 13:40:03 +0200

    werkt bijna
    da shit werkt bijna helemaal, moe alleen nog mar custom kolom namen
    invoegen

--- CSVToXML/CSVToXML/Form1.cs
+++ CSVToXML/CSVToXML/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 namespace CSVToXML
 {
     public partial class frmCSVToXML : Form
     {
+        private List<string> headers = new List<string>();
+        private int headerCounter = 0;
+
+        //CONSTRUCTORS
         public frmCSVToXML()
         {
             InitializeComponent();
+
+            headers.Add("Player_Name");
+            headers.Add("DollarBankroll");
+            headers.Add("EuroBankroll");
+            headers.Add("DollarMakeup");
+            headers.Add("EuroMakeup");
+        }
+
+        //METHODS
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var lines = File.ReadAllLines("Players.csv");
+
+                var xml = new XElement("TopElement", lines.Select(line => new XElement("Item", line.Split(',').Select((column, index) => new XElement(headers[headerCounter], column)))));
+
+                xml.Save("Players.xml");
+            }
+            catch (Exception b)
+            {
+                throw b;
+            }
         }
     }
 }
